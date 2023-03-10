c7567d63b2dbdb04f8acc5edcb5301dbaf69c515
“Priyam <priyamgupta990@gmail.com>
2023-03-10 18:59:12 +0530

    v1.0

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Card = ({name , link, deleteCard ,index}) => {
+const Card = ({name, link, deleteCard, editCard, moveCard, playCard, index}) => {
   return (
     <div className="card">
         <div>
@@ -8,6 +8,9 @@ const Card = ({name , link, deleteCard ,index}) => {
         <span>{link}</span>
         </div>
         <button onClick={() => deleteCard(index)}>-</button>
+      <button onClick={() => editCard(index)}>Edit</button>
+      <button onClick={() => moveCard(index)}>Move</button>
+      <button onClick={() => playCard(link)}>Play</button>
     </div>
   )
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -13,55 +13,162 @@ const Home = () => {
     const [name ,setName] = useState("");
     const [link ,setLink] = useState("");
     // initial value is given empty array in useState function.
-
-    const deleteCard =(index) => {
-        const filteredArr = cards.filter((val ,i) => {
-          return i !== index;
-        })
-        setCards(filteredArr);
-        console.log(filteredArr);
-        // filteredArr gives the tasks that are left in the array after removing the tasks.
-    } 
-
-    const submithandler = (e) => {
-        e.preventDefault();
-
-        setCards([...cards,{
-          name, link
-        },])
-        setName("")
-        setLink("")
-    
-        // First added the tasks that are present initially & then after tasks are added manually
-        console.log(cards)
-        // tasks array elements get come after the ADD button.
-    }
+    const [buckets, setBuckets] = useState([]);
+    const [selectedBucket, setSelectedBucket] = useState('');
 
     useEffect(() => {
-        localStorage.setItem("cards" , JSON.stringify(cards))
-      },[cards]) 
-
-    return (
-        <div className="container">
-        <h1 >Cards Section</h1>
-        <form onSubmit={submithandler} >
-          <input type="text" placeholder='Notes Heading...' value={name} onChange={(e) =>
-            setName(e.target.value)} />
-          <textarea placeholder='Notes Desc...' value={link} onChange={(e) =>
-            setLink(e.target.value)}></textarea>
-          <button type="submit" >Click me!!! Notes added..</button>
+      localStorage.setItem("cards" , JSON.stringify(cards))
+    },[cards]) 
   
-          {cards.map((card,index) => {
-            // Here item or the first parameter says the first task in array
-            // Every task has two things one is title and other is description.
-             return <Card key={index} name={card.name} link={card.link} deleteCard={deleteCard}
-             index ={index} />
-          })}
-          {/* When we use map function we hv to give unique key */}
-          {/* Map function in Javascript written in curly brackets any jaascript line should be written in curly brackets*/}
-        </form>
-      </div>
-  )
-}
+    useEffect(() => {
+      const bucketNames = cards.reduce((acc, curr) => {
+        if (!acc.includes(curr.bucket)) {
+          acc.push(curr.bucket);
+        }
+        return acc;
+      }, []);
+      setBuckets(bucketNames);
+    }, [cards]);
 
-export default Home
+    const addCard = (e) => {
+      e.preventDefault();
+      if (!selectedBucket) {   // If the bucket is not present alert
+        alert('Please make a bucket before adding a card');
+        setName('');
+        setLink('');
+        return;
+      }
+      const newCard = { name, link, bucket: selectedBucket };
+      setCards([...cards, newCard]);
+      setName('');
+      setLink('');
+      console.log(cards)
+    };
+
+  const deleteCard = index => {
+      const filteredArr = cards.filter((_, i) => i !== index);
+      setCards(filteredArr);
+  };
+
+  const editCard = (index, newName, newLink) => {
+    const updatedCards = cards.map((card, i) => {
+      if (i === index) {
+        return { ...card, name: newName, link: newLink };
+      }
+      return card;
+    });
+    setCards(updatedCards);
+  };
+
+  const deleteAllCards = () => {
+    const filteredArr = cards.filter(card => card.bucket !== selectedBucket);
+    setCards(filteredArr);
+  };
+
+  const selectBucket = e => {   
+    setSelectedBucket(e.target.value);
+  };
+
+  const createBucket = e => {     // To add a new bucket in the buckets array
+    e.preventDefault();
+    if (selectedBucket) {
+      setBuckets([...buckets, selectedBucket]);
+      setSelectedBucket('');
+    }
+  };
+  const moveCard = (index, newBucket) => {
+    const updatedCards = cards.map((card, i) => {
+      if (i === index) {
+        return { ...card, bucket: newBucket };
+      }
+      return card;
+    });
+    setCards(updatedCards);
+  };
+
+  const playCard = link => {
+    const playedCard = { name: name, link: link, time: new Date().toLocaleString() };
+    const history = localStorage.getItem('history')
+      ? JSON.parse(localStorage.getItem('history'))
+      : [];
+    localStorage.setItem('history', JSON.stringify([...history, playedCard]));
+    window.open(link, '_blank');
+  };
+
+  const history = localStorage.getItem('history')
+    ? JSON.parse(localStorage.getItem('history'))
+    : [];
+
+  return (
+    <div className="container">
+      <h1>Cards Section</h1>
+      <form onSubmit={addCard}>
+        <input type="text" placeholder="Card Heading..." value={name} onChange={e => setName(e.target.value)} />
+        <input type="text" placeholder="Video/MP3 Link..." value={link} onChange={e => setLink(e.target.value)} />
+        <select value={selectedBucket} onChange={selectBucket}>
+          <option disabled value="">
+            Select a bucket
+          </option>
+          {buckets.map(bucket => (
+            <option key={bucket} value={bucket}>
+              {bucket}
+            </option>
+          ))}
+        </select>
+        <button type="submit">Add Card</button>
+      </form>
+      <form onSubmit={createBucket}>
+        <input type="text" placeholder="Bucket Name..." value={selectedBucket} onChange={e => setSelectedBucket(e.target.value)} />
+        <button type="submit">Create Bucket</button>
+      </form>
+      {buckets.length > 0 && (
+        <div>
+          <h2>Buckets:</h2>
+          <ul>
+          {buckets.map((buck, index) => (
+              <li key={buck}>
+                {buck}{' '}
+                <button onClick={() => deleteAllCards(index)}>Delete All</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      {cards.length > 0 && (
+        <div>
+          <h2>Cards:</h2>
+          {cards.map((card, index) => {
+            if (!selectedBucket || card.bucket === selectedBucket) {
+              return (
+                <Card
+                  key={index}
+                  name={card.name}
+                  link={card.link}
+                  deleteCard={() => deleteCard(index)}
+                  editCard={(newName, newLink) => editCard(index, newName, newLink)}
+                  moveCard={newBucket => moveCard(index, newBucket)}
+                  playCard={() => playCard(card.link)}
+                />
+              );
+            }
+            return null;
+          })}
+        </div>
+      )}
+      {history.length > 0 && (
+        <div>
+          <h2>History:</h2>
+          <ul>
+            {history.map((playedCard, index) => (
+              <li key={index}>
+                {playedCard.name} - {playedCard.time}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
