a8723691f014e1327fc53982a4c2d58571db53fa
“Priyam <priyamgupta990@gmail.com>
2023-03-10 23:15:53 +0530

    Edit Functionality improved

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,39 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Card = ({name, link, deleteCard, editCard, moveCard, playCard, index}) => {
+
+  const [newName, setNewName] = useState(name);
+  const [newLink, setNewLink] = useState(link);
+  const [isEditing, setIsEditing] = useState(false);
+  
+    const handleEdit = e => {
+      e.preventDefault();
+      editCard(index, newName, newLink);
+      setIsEditing(false);
+    };
+  
   return (
     <div className="card">
-        <div>
-        <p>{name}</p>
-        <span>{link}</span>
-        </div>
-      <button onClick={() => deleteCard(index)}>-</button>
-      <button onClick={() => editCard(index)}>Edit</button>
-      <button onClick={() => moveCard(index)}>Move</button>
-      <button onClick={() => playCard(link)}>Play</button>
+    <div>
+      {isEditing ? (
+        <form onSubmit={handleEdit}>
+          <input type="text" value={newName} onChange={e => setNewName(e.target.value)} />
+          <input type="text" value={newLink} onChange={e => setNewLink(e.target.value)} />
+          <button type="submit">Save</button>
+        </form>
+      ) : (
+        <>
+          <p>{newName}</p>
+          <span>{newLink}</span>
+        </>
+      )}
     </div>
+    <button onClick={() => deleteCard(index)}>-</button>
+    <button onClick={() => setIsEditing(true)}>Edit</button>
+    <button onClick={() => moveCard(index)}>Move</button>
+    <button onClick={() => playCard(link)}>Play</button>
+  </div>
   )
 }
 
